900190d2ca9f37a1e89fe3244baf9d56fc13cddb
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2023-11-27 17:28:36 +0000

    asd

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
+var produtos = []
+var totalPagar = 
+
 function adicionar1() {
     var quantidade = document.getElementById("quantidade1").value
     var total = quantidade * 10
+    produtos.push(total)
     console.log(total);
 }
 
 function adicionar2() {
     var quantidade = document.getElementById("quantidade2").value
     var total = quantidade * 20
+    produtos.push(total)
     console.log(total);
 }
 
 function adicionar3() {
     var quantidade = document.getElementById("quantidade3").value
     var total = quantidade * 30
+    produtos.push(total)
     console.log(total);
 }
 
 function adicionar4() {
     var quantidade = document.getElementById("quantidade4").value
     var total = quantidade * 40
+    produtos.push(total)
     console.log(total);
 }
 
 function adicionar5() {
     var quantidade = document.getElementById("quantidade5").value
     var total = quantidade * 50
+    produtos.push(total)
     console.log(total);
 }
 function adicionar6() {
     var quantidade = document.getElementById("quantidade6").value
     var total = quantidade * 60
+    produtos.push(total)
     console.log(total);
 }
+
+function carrinho() {
+    
+    for (let i = 0; i < produtos.length; i++) {
+        var totalPagar = totalPagar + produtos[i];
+    }
+
+    document.write(totalPagar);
+    
+}
+
