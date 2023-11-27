0f6f13b607e0822151fdb44d9a4d0c58938a7979
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2023-11-27 16:41:21 +0000

    asd
    Co-authored-by: amandaprigol <amandaprigol@users.noreply.github.com>
    Co-authored-by: kaylaflach9955 <kaylaflach9955@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function adicionar() {
+function adicionar1() {
     var quantidade = document.getElementById("quantidade1").value
+    var total = quantidade * 10
+    console.log(total);
+}
+
+function adicionar2() {
+    var quantidade = document.getElementById("quantidade2").value
+    var total = quantidade * 20
+    console.log(total);
+}
+
+function adicionar3() {
+    var quantidade = document.getElementById("quantidade3").value
+    var total = quantidade * 30
+    console.log(total);
+}
+
+function adicionar4() {
+    var quantidade = document.getElementById("quantidade4").value
     var total = quantidade * 40
+    console.log(total);
+}
+
+function adicionar5() {
+    var quantidade = document.getElementById("quantidade5").value
+    var total = quantidade * 50
+    console.log(total);
+}
+function adicionar6() {
+    var quantidade = document.getElementById("quantidade6").value
+    var total = quantidade * 60
     console.log(total);
 }
