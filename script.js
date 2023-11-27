e2cd5092dd40e5780d2e94d1f513a99c8adae95d
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2023-11-27 21:59:21 +0000

    asd

--- script.js
+++ script.js
@@ -1,51 +1,45 @@
 var produtos = []
 var totalPagar = 
-var listaQuantidade = []
 
 
-function adicionar1() {
+function Adicionar1() {
     var quantidade = document.getElementById("quantidade1").value
     var total = quantidade * 10
-    listaQuantidade.push(quantidade)
     produtos.push(total)
     console.log(total);
 }
 
-function adicionar2() {
+function Adicionar2() {
     var quantidade = document.getElementById("quantidade2").value
     var total = quantidade * 20
-    listaQuantidade.push(quantidade)
+
     produtos.push(total)
     console.log(total);
 }
 
-function adicionar3() {
+function Adicionar3() {
     var quantidade = document.getElementById("quantidade3").value
     var total = quantidade * 30
-    listaQuantidade.push(quantidade)
     produtos.push(total)
     console.log(total);
 }
 
-function adicionar4() {
+function Adicionar4() {
     var quantidade = document.getElementById("quantidade4").value
     var total = quantidade * 40
-    listaQuantidade.push(quantidade)
     produtos.push(total)
     console.log(total);
 }
 
-function adicionar5() {
+function Adicionar5() {
     var quantidade = document.getElementById("quantidade5").value
     var total = quantidade * 50
-    listaQuantidade.push(quantidade)
     produtos.push(total)
     console.log(total);
 }
-function adicionar6() {
+function Adicionar6() {
     var quantidade = document.getElementById("quantidade6").value
     var total = quantidade * 60
-    listaQuantidade.push(quantidade)
     produtos.push(total)
     console.log(total);
 }
@@ -57,10 +51,6 @@ function carrinho() {
     for (let i = 0; i < produtos.length; i++) {
         totalPagar = totalPagar + parseInt(produtos[i]);
     }
-
-    document.write(produtos + "<br>") ;
-    document.write(totalPagar);
-    document.write(listaQuantidade);
     
 }
 
